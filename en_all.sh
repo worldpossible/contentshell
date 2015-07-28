690d609c749da3b86e6342f68bf400735aea36da
Jeremy Schwartz <jeremy@worldpossible.org>
2015-07-28 11:32:21 -0700

    Update en_all.sh

--- en_all.sh
+++ en_all.sh
@@ -5,6 +5,7 @@ sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet ./
 #uncomment below if not using KA-Lite
 #sudo rsync -av sudo rsync://dev.worldpossible.org/rachelmods/kaos-en ./
 #sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/khan_health ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ka-lite ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory ./
@@ -15,6 +16,7 @@ sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra ./
 #uncomment below if not using kiwix Wikipedia
 #sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wiki-en ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ck12 ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps ./
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/PhET ./
