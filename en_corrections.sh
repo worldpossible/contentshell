efdcb045d371e135edb64b2804c1674ad99a331a
Jeremy Schwartz <jeremy@worldpossible.org>
2015-07-29 14:16:58 -0700

    Create en_corrections.sh

---
+++ en_corrections.sh
@@ -0,0 +1,24 @@
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ebooks-en ./
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/hesperian_health ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/iicba ./
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet ./
+#uncomment below if not using KA-Lite
+#sudo rsync -av sudo rsync://dev.worldpossible.org/rachelmods/kaos-en ./
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/khan_health ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ka-lite ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression ./
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/olpc ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/powertyping ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/practical_action ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools ./
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wiki_en ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ck12 ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/PhET ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/TED ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/radiolab ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/sphider_plus.sql ./
