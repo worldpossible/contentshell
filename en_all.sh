d4417fb8a0878ed0eac618f3037e1ec1cfb28187
Jeremy Schwartz <jeremy@worldpossible.org>
2015-07-31 10:30:13 -0700

    Update en_all.sh

--- en_all.sh
+++ en_all.sh
@@ -1,26 +1,47 @@
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ebooks-en ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/hesperian_health ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/iicba ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ebooks-en /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/hesperian_health /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/iicba /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/infonet /media/RACHEL/rachel/modules/
+
 #uncomment below if not using KA-Lite
-#sudo rsync -av sudo rsync://dev.worldpossible.org/rachelmods/kaos-en ./
-#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/khan_health ./
+#sudo rsync -av sudo rsync://dev.worldpossible.org/rachelmods/kaos-en /media/RACHEL/rachel/modules/
+
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/khan_health /media/RACHEL/rachel/modules/
+
 sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ka-lite /media/RACHEL/rachel/modules/
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/olpc ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/powertyping ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/practical_action ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra ./
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/math_expression /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/medline_plus /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/musictheory /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/olpc /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/powertyping /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/practical_action /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/scratch /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/understanding_algebra /media/RACHEL/rachel/modules/
+
 #uncomment below if not using kiwix Wikipedia
-#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wiki_en ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ck12 ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/PhET ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/TED ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/radiolab ./
-sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/sphider_plus.sql ./
+#sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wikipedia_for_schools /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/wiki_en /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/ck12 /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/windows_apps /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/PhET /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/TED /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/radiolab /media/RACHEL/rachel/modules/
+
+sudo rsync -avz rsync://dev.worldpossible.org/rachelmods/sphider_plus.sql /media/RACHEL/rachel/modules/
 
