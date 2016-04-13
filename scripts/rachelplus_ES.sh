214dbdd6a0d3e7f6036dd28dfdf9a3d4b1d01d44
Jeremy Schwartz <jeremy@worldpossible.org>
2016-04-13 13:34:55 -0700

    Update rachelplus_ES.sh

--- scripts/rachelplus_ES.sh
+++ scripts/rachelplus_ES.sh
@@ -15,11 +15,19 @@ rsync -Pavz rsync://dev.worldpossible.org/rachelmods/ap_didact_es /media/RACHEL/
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/cnbguatemala-es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/file_share-es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/kalite-es /media/RACHEL/rachel/modules/.
+#enable spanish as language in kalite
+cp -r /usr/share/kalite/data/khan/topics.json /usr/share/kalite/data/khan/topics.json.bak 
+sudo rm -r /usr/share/kalite/data/khan/topics.json
+cp -r /media/RACHEL/rachel/modules/kalite-es/topics.json /usr/share/kalite/data/khan/
+cp -r /root/.kalite/locale/dubbed_video_mappings.json /root/.kalite/locale/dubbed_video_mappings.json.bak
+cp -r /media/RACHEL/rachel/modules/kalite-es/dubbed_video_mappings.json /root/.kalite/locale/   
+ln -s /media/RACHEL/rachel/modules/kalite-es/content/* /media/RACHEL/kacontent/.
+
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/educalab-es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/blockly_games-es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/wikivoyage-es /media/RACHEL/rachel/modules/.
 /var/kiwix/bin/kiwix-manage /media/RACHEL/kiwix/data/library/library.xml  add /media/RACHEL/rachel/modules/wikivoyage-es/data/content/wikivoyage_es_all_2016-04.zim  --indexPath=/media/RACHEL/rachel/modules/wikivoyage-es/data/index/wikivoyage_es_all_2016-04.zim.idx
 sudo cp /media/RACHEL/rachel/modules/wikivoyage-es/rachel-index.php /media/RACHEL/rachel/modules/wikivoyage-es/index.htmlf
-
+rsync -Pavz rsync://dev.worldpossible.org/rachelmods/admin.es.cap.sqlite /media/RACHEL/rachel/admin.sqlite
 
 ##wikivoyage and wikipedia must be added to kiwix library
