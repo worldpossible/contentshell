599dd39120d52bd8eb7d00378b930f73ca3fc90c
Jeremy Schwartz <jeremy@worldpossible.org>
2016-04-13 12:57:11 -0700

    Update rachelplus_ES.sh

--- scripts/rachelplus_ES.sh
+++ scripts/rachelplus_ES.sh
@@ -17,7 +17,6 @@ rsync -Pavz rsync://dev.worldpossible.org/rachelmods/file_share-es /media/RACHEL
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/kalite-es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/educalab-es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/blockly_games-es /media/RACHEL/rachel/modules/.
-rsync -Pavz rsync://dev.worldpossible.org/rachelmods/educalab_es /media/RACHEL/rachel/modules/.
 rsync -Pavz rsync://dev.worldpossible.org/rachelmods/wikivoyage-es /media/RACHEL/rachel/modules/.
 /var/kiwix/bin/kiwix-manage /media/RACHEL/kiwix/data/library/library.xml  add /media/RACHEL/rachel/modules/wikivoyage-es/data/content/wikivoyage_es_all_2016-04.zim  --indexPath=/media/RACHEL/rachel/modules/wikivoyage-es/data/index/wikivoyage_es_all_2016-04.zim.idx
 sudo cp /media/RACHEL/rachel/modules/wikivoyage-es/rachel-index.php /media/RACHEL/rachel/modules/wikivoyage-es/index.htmlf
