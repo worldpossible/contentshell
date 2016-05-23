d459b2d9a9134f9e18f588652362a37088e1e5ed
Jeremy Schwartz <jeremy@worldpossible.org>
2016-05-23 16:02:54 -0700

    Update rachelplus_EN_local.sh

--- scripts/rachelplus_EN_local.sh
+++ scripts/rachelplus_EN_local.sh
@@ -13,9 +13,13 @@ sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-musictheory /media/R
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-olpc /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-practical_action /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-understanding_algebra /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-PhET /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-radiolab /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-powertyping /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-iicba /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikibooks /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikipedia_for_schools /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikisource /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikiversity /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikivoyage /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wiktionary /media/RACHEL/rachel/modules/.
@@ -23,4 +27,10 @@ sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-windows_apps /media/
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-worldmap /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-kalite /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikipedia /media/RACHEL/rachel/modules/.
-
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-oya /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-law_library /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-fantastic_phonics /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/en-wikipedia /media/RACHEL/rachel/modules/.
+sudo mkdir /media/RACHEL/kacontent
+sudo mv /media/RACHEL/rachel/modules/en-kalite/content/* /media/RACHEL/kacontent/.
+sudo mv /media/RACHEL/rachel/modules/en-kalite/content
