44774c271ce484f871f1ad994208109a6cdc5997
Jeremy Schwartz <jeremy@worldpossible.org>
2016-05-27 10:05:21 -0700

    Update FR_plus_local.sh

--- scripts/FR_plus_local.sh
+++ scripts/FR_plus_local.sh
@@ -1,1 +1,12 @@
-
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-ebooksgratuits /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-haitifutur /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-phet-haiti /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wikipedia /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wikibooks /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wikipedia /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wikisource /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wikiversity /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wikivoyage /media/RACHEL/rachel/modules/.
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-wiktionary /media/RACHEL/rachel/modules/.
+sudo mkdir /media/RACHEL/kacontent
+sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-kalite/content/* /media/RACHEL/kacontent/.
