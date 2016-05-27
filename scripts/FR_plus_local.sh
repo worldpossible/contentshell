1a81f4862ff28547367c8b858dd2cee786bf5d9a
Jeremy Schwartz <jeremy@worldpossible.org>
2016-05-27 10:18:14 -0700

    Update FR_plus_local.sh

--- scripts/FR_plus_local.sh
+++ scripts/FR_plus_local.sh
@@ -1,3 +1,8 @@
+sudo rm -r /media/RACHEL/rachel/modules/en-TED/TEDx*
+sudo rm -r /media/RACHEL/rachel/modules/en-TED/TED@*
+sudo rm -r /media/RACHEL/rachel/modules/en-TED/TEDSalo*
+sudo rm -r /media/RACHEL/rachel/modules/en-TED/TEDIndi*
+
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-ebooksgratuits /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-haitifutur /media/RACHEL/rachel/modules/.
 sudo rsync -Pavz rsync://192.168.1.74/rachelmods/modules/fr-phet-haiti /media/RACHEL/rachel/modules/.
