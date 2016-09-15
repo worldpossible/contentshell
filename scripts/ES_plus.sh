8dc0cd22dff183f3cdf615acbd0858797a5fca76
Jonathan Field <jfield@gmail.com>
2016-09-14 20:15:43 -0700

    added --del to rsync so changed modules get cleaned up

--- scripts/ES_plus.sh
+++ scripts/ES_plus.sh
@@ -10,20 +10,20 @@ fi
 
 # files that are already highly compressed (zim, videos) will probably
 # transfer faster without the -z option (which uses zip compression)
-rsync -av  rsync://$1/rachelmods/es-kalite /media/RACHEL/rachel/modules/
-rsync -av  rsync://$1/rachelmods/es-wikipedia /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-wikihow /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-ap_didact /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-biblioteca /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-blockly_games /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-cnbguatemala /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-ebooks /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-educalab /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-file_share /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-guatemala /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-guias /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-hesperian_health /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-medline_plus /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-soluciones /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-vedoque /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/es-windows_apps /media/RACHEL/rachel/modules/
+rsync -av  --del rsync://$1/rachelmods/es-kalite /media/RACHEL/rachel/modules/
+rsync -av  --del rsync://$1/rachelmods/es-wikipedia /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-wikihow /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-ap_didact /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-biblioteca /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-blockly_games /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-cnbguatemala /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-ebooks /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-educalab /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-file_share /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-guatemala /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-guias /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-hesperian_health /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-medline_plus /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-soluciones /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-vedoque /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/es-windows_apps /media/RACHEL/rachel/modules/
--- scripts/FR_plus.sh
+++ scripts/FR_plus.sh
@@ -10,9 +10,9 @@ fi
 
 # files that are already highly compressed (zim, videos) will probably
 # transfer faster without the -z option (which uses zip compression)
-rsync -av  rsync://$1/rachelmods/fr-kalite /media/RACHEL/rachel/modules/
-rsync -av  rsync://$1/rachelmods/fr-wikipedia /media/RACHEL/rachel/modules/
-rsync -av  rsync://$1/rachelmods/fr-wikivoyage /media/RACHEL/rachel/modules/
-rsync -av  rsync://$1/rachelmods/fr-wiktionary /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/fr-ebooksgratuits /media/RACHEL/rachel/modules/
-rsync -avz rsync://$1/rachelmods/fr-haitifutur /media/RACHEL/rachel/modules/
+rsync -av  --del rsync://$1/rachelmods/fr-kalite /media/RACHEL/rachel/modules/
+rsync -av  --del rsync://$1/rachelmods/fr-wikipedia /media/RACHEL/rachel/modules/
+rsync -av  --del rsync://$1/rachelmods/fr-wikivoyage /media/RACHEL/rachel/modules/
+rsync -av  --del rsync://$1/rachelmods/fr-wiktionary /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/fr-ebooksgratuits /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/fr-haitifutur /media/RACHEL/rachel/modules/
