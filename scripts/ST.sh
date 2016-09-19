17b6f5c2d92784923727b72edff53ae510a0a0c6
Jeremy Schwartz <jeremy@worldpossible.org>
2016-09-19 11:48:19 -0700

    Update ST.sh

--- scripts/ST.sh
+++ scripts/ST.sh
@@ -13,5 +13,17 @@ fi
 set -x # echo commands
 
 echo 'this should be run after running the base script'
+echo 'removing unused modules'
+sudo rm -r /media/RACHEL/rachel/modules/en-fairshake
+sudo rm -r /media/RACHEL/rachel/modules/en-scratch
+sudo rm -r /media/RACHEL/rachel/modules/en-law_library
+sudo rm -r /media/RACHEL/rachel/modules/es-kalite
+sudo rm -r /media/RACHEL/rachel/modules/es-wikipedia
+sudo rm -r /media/RACHEL/rachel/modules/es-windows_apps
 
+echo 'adding custom modules for ST'
 rsync -avz --del rsync://$1/rachelmods/fr-kaos /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/fr-phet-haiti /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/fr-wikibooks /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/en-wikibooks /media/RACHEL/rachel/modules/
+rsync -avz --del rsync://$1/rachelmods/fr-wikiversity /media/RACHEL/rachel/modules/
