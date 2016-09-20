c74ed9c868f69b8ddd88b7514547326c1feac8e1
Jeremy Schwartz <jeremy@worldpossible.org>
2016-09-20 13:54:01 -0700

    Update ST.sh

--- scripts/ST.sh
+++ scripts/ST.sh
@@ -29,7 +29,7 @@ rsync -avz --del rsync://$1/rachelmods/en-wikibooks /media/RACHEL/rachel/modules
 rsync -avz --del rsync://$1/rachelmods/fr-wikiversity /media/RACHEL/rachel/modules/
 
 echo 'bring in admin.sqlite'
-rsync -avz --del rsync://192.168.1.74/rachelmods/admin.sqlite/st.admin.sqlite /media/RACHEL/rachel/admin.sqlite
+rsync -avz --del rsync://$1/rachelmods/extra-build-files/st.admin.sqlite /media/RACHEL/rachel/admin.sqlite
 
 echo 'df -h'
 cd $HOME
