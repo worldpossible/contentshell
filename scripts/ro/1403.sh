924735663115ce18b395fd5134f880c2ddadca63
Jeremy Schwartz <jeremy@worldpossible.org>
2016-10-06 23:12:00 -0700

    Update 1403.sh

--- scripts/ro/1403.sh
+++ scripts/ro/1403.sh
@@ -8,14 +8,17 @@ sudo rm -r /media/RACHEL/rachel/modules/en-windows_apps
 sudo rm -r /media/RACHEL/rachel/modules/es-wikipedia
 sudo rm -r /media/RACHEL/rachel/modules/fr-wikipedia
 sudo rm -r /media/RACHEL/rachel/modules/es-hesperian_health
-sudo rm -r /media/RACHEL/rachel/modules/en-medline_plus
+sudo rm -r /media/RACHEL/rachel/modules/es-medline_plus
+sudo rm -r /media/RACHEL/rachel/modules/es-windows_apps
 
-rsync -Pavz rsync://dev.worldpossible.org/rachelmods/en-saylor /media/RACHEL/rachel/modules/.
-rsync -Pavz rsync://dev.worldpossible.org/rachelmods/en-fairshake /media/RACHEL/rachel/modules/.
-rsync -Pavz rsync://dev.worldpossible.org/rachelmods/en-oya /media/RACHEL/rachel/modules/.
-rsync -Pavz rsync://dev.worldpossible.org/rachelmods/en-law_library /media/RACHEL/rachel/modules/.
+rsync -Pavz rsync://192.168.1.74/rachelmods/en-saylor /media/RACHEL/rachel/modules/.
+rsync -Pavz rsync://192.168.1.74/rachelmods/en-fairshake /media/RACHEL/rachel/modules/.
+rsync -Pavz rsync://192.168.1.74/rachelmods/en-oya /media/RACHEL/rachel/modules/.
+rsync -Pavz rsync://192.168.1.74/rachelmods/en-law_library /media/RACHEL/rachel/modules/.
 
 #install weaved
 echo 'install weaved'
 
 echo 'put the modules files somewhere'
+
+echo 'check how much storage is free df -h'
