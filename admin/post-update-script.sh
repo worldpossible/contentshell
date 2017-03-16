c82e8eab362d8c6462a1f1a2868cf6c972abfb0e
Jonathan Field <jfield@worldpossible.org>
2017-03-16 14:29:22 +0200

    initial (no-op) post-update script

--- admin/post-update-script.sh
+++ admin/post-update-script.sh
@@ -8,3 +8,58 @@
 #   3. The script should complete quickly and exit cleanly
 #
 # This takes some doing, but it's important
+main() {
+
+    setVariables
+    checkVariables
+
+}
+
+# set up some globals that will be useful later
+setVariables() {
+
+    # these directories function as flags to identify the system
+    plusWebDir=/media/RACHEL/rachel
+    piWebDir=/var/www/rachel
+
+    # check which system we're on and set variables accordingly
+    if [[ -d $plusWebDir ]]; then
+        # we're on a RACHEL-Plus
+        isPlus=1
+        webDir=$plusWebDir
+    elif [[ -d $piWebDir ]]; then
+        # we're on a RACHEL-Pi
+        isPi=1
+        webDir=$piWebDir
+    else
+        echo "Unknown System -- exiting"
+        exit 1
+    fi
+
+    # vars that are the same on both go here
+    scriptDir=/root/rachel-scripts
+
+}
+
+checkVariables() {
+
+    if [[ $isPlus ]]; then
+        echo "isPlus: " $isPlus;
+    fi
+    if [[ $isPi ]]; then
+        echo "isPi: " $isPi;
+    fi
+
+    echo "webDir: " $webDir;
+    echo "scriptDir: " $scriptDir;
+
+}
+
+# install our remote management software if needed
+installESP() {
+    if [[ $isRachelPlus ]]; then
+        echo "Not Implemented... skipping";
+    fi
+}
+
+main "$@"
