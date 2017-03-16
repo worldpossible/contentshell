38492b9ad62b9412ed9b76166e2f100e4f773243
Jonathan Field <jfield@worldpossible.org>
2017-03-16 14:47:25 +0200

    initial command-line module installation script

---
+++ installmods.php
@@ -0,0 +1,44 @@
+<?php
+
+# This script installs, then sorts & hides modules
+# according to a given .modules file
+
+$dir = dirname(__FILE__) . "/scripts/";
+
+if (isset($argv[1]) && isset($argv[2])) {
+
+    $file = "$dir/$argv[1].modules";
+
+    if (is_readable($file)) {
+
+        # figure out where we're getting stuff from
+        switch ($argv[2]) {
+            case "dev":
+                $server = "dev.worldpossible.org";
+                break;
+            case "jeremy":
+                $server = "192.168.1.74";
+                break;
+            case "jfield":
+                $server = "192.168.1.6";
+                break;
+            default:
+                $server = $argv[2];
+        }
+
+        require_once("admin/common.php");
+        sortmods($file, $server);
+        exit(0);
+
+    }
+
+}
+
+echo "Usage: php installmods.php modulesfile server\n";
+exit(1);
+
+function installmods() {
+    
+}
+
+?>
