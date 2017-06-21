3bb9907b8e961200672cc7bd5252da99f9f3e88c
Jonathan Field <jfield@worldpossible.org>
2017-06-20 22:29:58 -0700

    mostly improvements to install and update

---
+++ admin/do_tasks.php
@@ -0,0 +1,323 @@
+<?php
+
+#-------------------------------------------
+# This script reads tasks from a queue in the
+# database and runs them. As they run, it updates
+# the # database with their output.
+# As a special case with rsync, it parses some
+# of the output to record progress.
+#
+# Usage: do_tasks.php
+#-------------------------------------------
+
+require_once("common.php");
+
+define("VERBOSE", false);
+ini_set("error_log", "syslog"); # should this go to the apache log?
+$relmodpath = getrelmodpath(); # needed to run finish_install.sh
+
+error_log("starting tasks");
+
+#-------------------------------------------
+# input checking - there should be no input
+#-------------------------------------------
+if (!empty($argv[1])) {
+    error_log("arguments ignored... exiting.");
+    exit(1);
+}
+
+#-------------------------------------------
+# pid checking - if there's already a copy running, we bail
+#-------------------------------------------
+$pidfile = "/var/run/rachel_do_tasks.pid";
+if (file_exists($pidfile)) {
+    # we verify that the pid really is running
+    $pid = preg_replace("/\D+/", "", file_get_contents($pidfile));
+    $output = exec("ps $pid | sed 1d");
+    if (!empty($output)) {
+        error_log("already running... exiting.");
+        exit(0);
+    } else {
+        # something went wrong, let's try again
+        error_log("pidfile found but no process... restarting.");
+        unlink($pidfile);
+    }
+}
+
+# we're clear - let's run
+file_put_contents($pidfile, getmypid());
+
+# make sure we clear our pid file when we finish
+function do_tasks_cleanup() {
+    global $pidfile;
+    unlink($pidfile);
+}
+register_shutdown_function('do_tasks_cleanup');
+
+# grab a database connection
+$db = getdb();
+
+#-------------------------------------------
+# we run tasks from the queue until they're all done
+#-------------------------------------------
+$last_task_id = 0;
+while (true) {
+
+    # check for a task
+    $next = $db->querySingle("
+        SELECT task_id, moddir, command
+          FROM tasks
+         WHERE pid IS NULL
+           AND dismissed IS NULL
+         ORDER BY task_id LIMIT 1
+    ", true);
+
+    # if we're done, we're done
+    if (!$next) {
+        error_log("no more tasks; exiting.");
+        break;
+    }
+
+    error_log("running task $next[task_id] ($next[moddir])");
+
+    # detect looping on the same process
+    if ($next['task_id'] == $last_task_id) {
+        error_log("apparently stuck on task_id $last_task_id; exiting.");
+        exit(1);
+    }
+    $last_task_id = $next['task_id'];
+
+    # gather our stuff
+    $db_task_id = $next['task_id'];
+    $cmd        = $next['command'];
+    $moddir     = $next['moddir'];
+
+    #-------------------------------------------
+    # here we actually fire off the process and see what happens 
+    #-------------------------------------------
+    if (VERBOSE) { error_log("opening process: $cmd"); }
+    $db_started = $db->escapeString(time());
+
+    $pipedefs = array(
+       0 => array("pipe", "r"), # stdin
+       1 => array("pipe", "w"), # stdout
+       2 => array("pipe", "w")  # stderr
+    );
+    $proc = proc_open($cmd, $pipedefs, $pipes);
+    $info = proc_get_status($proc);
+
+    $db_pid         = $db->escapeString($info['pid']);
+    $db_started     = $db->escapeString(time());
+    $db_last_update = $db_started;
+    $db->exec("
+        UPDATE tasks SET
+            pid         = '$db_pid',
+            started     = '$db_started',
+            last_update = '$db_last_update',
+            files_done  = '0',
+            data_done   = '0',
+            data_rate   = ''
+        WHERE task_id = '$db_task_id'
+    ");
+
+    # if it failed, record and move on
+    if (!$info['running']) {
+        error_log("process completed instantly - task_id $db_task_id");
+        $db_stdout_tail = $db->escapeString(fread($pipes[1], 1024));
+        $db_stderr_tail = $db->escapeString(fread($pipes[2], 1024));
+        $db_retval      = $db->escapeString($info['exitcode']);
+        $db_completed   = $db_started;
+        $db->exec("
+            UPDATE tasks SET
+                completed   = '$db_completed',
+                last_update = '$db_last_update',
+                stdout_tail = '$db_stdout_tail',
+                stderr_tail = '$db_stderr_tail',
+                retval      = '$db_retval'
+            WHERE task_id = '$db_task_id'
+        ");
+        continue;
+    }
+
+    if (VERBOSE) { error_log("process opened - task_id $db_task_id - now reading"); }
+
+    #-------------------------------------------
+    # We keep reading from the pipe, one char at a time
+    # and store up lines, writing $tail_length at a time to
+    # the DB. We also limit ourselves to one write
+    # per $frequency seconds.
+    #-------------------------------------------
+    $line = "";        # - the current line, built one char at a time
+    $lines = array();  # - array of completed lines (limited to $tail_length)
+    $tail_length = 2;  # - only keep this many lines of output in $lines
+    $frequency = 2;    # - how often can we write to the db (in seconds)?
+    $cr = false;       # - flag: did the previous line have a carriage return?
+    $lastone = false;  # - flag: is this the last line of output?
+    $files_done = 0;   # - how many files have been completed? (from rsync)
+    $data_done = 0;    # - how much data in completed files? (from rsync)
+    $data_prog = 0;    # - how far along is the current file? (from rsync)
+    $data_rate = 0;    # - how fast is the transfer going? (from rsync)
+
+    # start reading
+    while (true) {
+
+        $char = fgetc($pipes[1]);
+
+        # we don't do this EOF test at the top of the loop
+        # because we need to send the last bits from
+        # the pipe to the database, so instead we set a flag
+        if (feof($pipes[1])) {
+            $lastone = true;
+            $line .= $char;
+        }
+
+        if ($char == "\n" || $char == "\r" || $lastone) {
+
+            # skip empty lines
+            if (preg_match("/\S/", $line)) {
+
+                # if the previous line had a carriage return
+                # it means we should replace instead of append
+                if ($cr) {
+                    if (count($lines) > 0) { array_pop( $lines ); }
+                    array_push($lines, $line);
+                    $cr = false;
+                } else {
+                    array_push($lines, $line);
+                    $lines = array_slice($lines, -$tail_length);
+                }
+
+                # Check this most recent line to see if it indicates
+                # file progress. If so, we count that and the data
+                # transferred. Note: some versions of rsync include
+                # commas in the numbers so we have to strip those out
+                if (preg_match("/^\s+([\d,]+)\s+(\d+)%\s+(\S+)/",
+                        $line, $matches)) {
+                    if ($matches[2] == "100") {
+                        # completed file, add it to the total
+                        $data_done += preg_replace("/,/", "", $matches[1]);
+                        ++$files_done;
+                    } else {
+                        # partial file in progress, keep track seperately
+                        $data_prog = preg_replace("/,/", "", $matches[1]);
+                    }
+                    $data_rate = $matches[3];
+                }
+
+            }
+
+            # we've recorded the line in the array, clear it
+            $line = "";
+
+            # keep track of whether this line should be replaced
+            if ($char == "\r") { $cr = true; }
+
+            # we will update the db with the latest output
+            # periodically, and also on our last output
+            if ( $db_last_update < time() - $frequency || $lastone ) {
+
+                $db_stdout_tail = $db->escapeString(implode("\n", $lines));
+                $db_last_update = $db->escapeString(time());
+                $cur_data_done = $data_done + $data_prog;
+                $db->exec("
+                    UPDATE tasks SET
+                        stdout_tail = '$db_stdout_tail',
+                        last_update = '$db_last_update',
+                        files_done  = '$files_done',
+                        data_done   = '$cur_data_done',
+                        data_rate   = '$data_rate'
+                    WHERE task_id = '$db_task_id'
+                ");
+                if (VERBOSE) { error_log("updating db with: '$db_stdout_tail'"); }
+
+            }
+
+            if ($lastone) { break; }
+
+        } else {
+            # just capture this character
+            $line .= $char;
+        }
+
+    }
+
+    #error_log("in lines array: " . implode("\n", $lines));
+    #error_log("in pipe: " . fread($pipes[2], 1024));
+
+    # grab whatever is left in stderr (up to 1 kb)
+    $db_stderr_tail = $db->escapeString(fread($pipes[2], 1024));
+
+    #-------------------------------------------
+    # we're done reading, so now we write the final results
+    # to the db, including the exitcode and any stderr output
+    #-------------------------------------------
+    # error_log("finished reading, calling fclose(0)");
+    fclose($pipes[0]);
+    # error_log("calling fclose(1)");
+    fclose($pipes[1]);
+    # error_log("calling fclose(2)");
+    fclose($pipes[2]);
+    if (VERBOSE) { error_log("pipes closed, calling proc_close()"); }
+
+    # close the process
+    $db_retval = $db->escapeString(proc_close($proc));
+
+    # run any installation script, if present
+    # XXX this needs better error checking and feedback
+    $path = "$relmodpath/$moddir";
+    $finscript = "finish_install.sh";
+    if (file_exists("$path/$finscript")) {
+        error_Log("running $path/$finscript");
+        $db->exec("
+            UPDATE tasks SET
+                stdout_tail = 'Starting: $path/$finscript',
+                last_update = '$db_last_update'
+            WHERE task_id = '$db_task_id'
+        ");
+        $output = array();
+        exec("cd $path && bash $finscript 2>&1", $output, $rval);
+        # put some record of our problem in the db
+        if ($rval == 0) {
+            $db->exec("
+                UPDATE tasks SET
+                    stdout_tail = 'Finished: $path/$finscript',
+                    last_update = '$db_last_update'
+                WHERE task_id = '$db_task_id'
+            ");
+        } else {
+            $db->exec("
+                UPDATE tasks SET
+                    stdout_tail = 'Failed: $path/$finscript',
+                    last_update = '$db_last_update'
+                WHERE task_id = '$db_task_id'
+            ");
+            $db_stderr_tail .= implode("\n", $output);
+            $db_retval = $rval;
+        }
+    }
+
+    if (VERBOSE) { error_log("updating db - task $db_task_id complete"); }
+    $db_completed = $db->escapeString(time());
+    $db->exec("
+        UPDATE tasks SET
+            completed = '$db_completed',
+            last_update = '$db_completed',
+            stderr_tail = '$db_stderr_tail',
+            retval = '$db_retval'
+        WHERE task_id = '$db_task_id'
+    ");
+
+}
+
+# restart kiwix so it sees what modules are visible/hidden
+# -- we could do this after each module but it seems a bit
+# much... let's try doing it after installs/updates are complete
+# and see if anyone complains. XXX the right way is to put this
+# in the the module's finish_install.sh
+if (VERBOSE) { error_Log("restarting kiwix..."); }
+kiwix_restart();
+
+if (VERBOSE) { error_Log("goodbye."); }
+exit(0);
+    
+?>
---
+++ admin/esp-checker.php
@@ -0,0 +1,253 @@
+<?php
+
+# FILE TAKEN FROM:
+# https://github.com/rachelproject/esp/blob/master/client/checker.php
+# release: v1.0.1 (b416c32)
+
+# checker.php
+# 
+# This script checks in with the dev server, then polls it
+# to see if we want it to connect. If it gets a request to
+# connect, it creates ssh tunnels for our desired ports, and
+# then manages those processes.
+#
+
+define("DEBUG", false);
+
+# some useful things to know
+$scriptpath =  dirname(__FILE__);
+$scriptname = basename(__FILE__);
+$scriptpid  = getmypid();
+
+# check we're the only one running or there will be blood
+$existing_pid = exec("ps x | grep $scriptname | grep -v $scriptpid | grep -v grep | awk '{ print $1 }'");
+if ($existing_pid) {
+    echo "$scriptname already running (pid: $existing_pid)... exiting.\n";
+    exit(1);
+}
+
+# this is the unique device ID. We use the last six of the eth0 mac address
+$id = exec("ifconfig | grep eth0 | awk '{ print $5 }' | sed s/://g | grep -o '.\{6\}$'");
+if (DEBUG) { echo  "Device ID: $id\n"; }
+
+# these are the ports we want to tunnel
+$ports = array(
+    "80",   # <- http
+    "81",   # <- kiwix
+    "8008", # <- kalite
+    "22"    # <- ssh
+);
+
+$host = "dev.worldpossible.org";
+$name = "esp";
+$url  = "http://$host/$name/server.php";
+$findflag = "$name-findflag"; # for finding our procs
+
+# we use these as sensible limits for when the
+# server tells us to use a different interval
+$std_interval =  20; # default polling rate, in seconds
+$min_interval =   5; # fast rate (while connected)
+$max_interval = 600; # slow rate (when offline)
+$interval = $std_interval;
+
+# check if there is already an ssh tunnel
+#if (DEBUG) { echo  "Startup check...\n"; }
+#$pids = getpids();
+#if ($pids) {
+#    if (DEBUG) { echo "Existing processes -- mark as conntected\n"; }
+#    $response = @file_get_contents( "$url?id=$id&connected=1" );
+#} else {
+#    if (DEBUG) { echo "No existing processes -- mark as disconntected\n"; }
+#    $response = @file_get_contents( "$url?id=$id&disconnected=1" );
+#}
+# 
+# whoops -- restarting with dynamic ports doesn't work... so:
+killpids();
+@file_get_contents( "$url?id=$id&disconnected=1" );
+
+# enter our infinite loop
+$network_fail = false;
+while (true) {
+
+    $response = @file_get_contents( "$url?id=$id" );
+
+    if ($response === false) {
+        $network_fail = true;
+        if (DEBUG) { echo "Can't contact server -- waiting $interval seconds.\n"; }
+        # This probably means we're not online - a common case, really.
+        sleep($interval);
+        # We back off our polling rate up to $max_interval. The reason we don't
+        # just jump straight to the max is because during startup network can
+        # take some time to come up, and so we want to keep trying quickly
+        # at first rather that go straight to a 10 minute timeout... but we'll get there
+        if ($interval < $max_interval) {
+            $interval += $std_interval;
+        }
+        continue;
+    } else if ($network_fail == true) {
+        # when we recover from a network fail, we go back to our
+        # standard polling rate -- even if the server had given
+        # us some other rate at some point
+        $network_fail = false;
+        $interval = $std_interval;
+    }
+
+    if (DEBUG) { echo "Server says: $response\n"; }
+
+    if ($response == "OK") {
+
+        # The most common response is just "OK" (with no number), which
+        # requires no action at all. Everything else is handled below:
+
+    } else if (preg_match("/OK (\d+)/", $response, $match)) {
+
+        # This means the server is requesting you change
+        # the polling frequency -- this allows us to tell
+        # devices to back off under load
+
+        $interval = $match[1];
+        # let's trust the server...
+        #if ($interval < $min_interval) {
+        #    $interval = $min_interval;
+        #} else if ($interval > $max_interval) {
+        #    $interval = $max_interval;
+        #}
+        # well, almost trust it...
+        if ($interval < 2) {
+            $interval = 2;
+        }
+        if (DEBUG) { echo "Polling interval set to: $interval\n"; }
+
+    } else if (preg_match("/CONNECT (\d+)/", $response, $match)) {
+
+        # This requests a connection -- we use the number provided
+        # as the starting point for the new port numbers
+        $offset = $match[1];
+
+        # clear out anything that's there
+        killpids();
+
+        # connect each port, using sequential numbers starting at the offset
+        $i = 0;
+        foreach ($ports as $rport) {
+            # a bit complex - we need to
+            # 1) specify the private key for login
+            # 2) add a harmless string that will let us find the ssh process
+            # 3) skip host checking
+            # 4) configure some things so that failed connections get closed
+            # 5) run the actual revesre tunnel on the right port
+            # 6) send all output to the dustbin so PHP can return
+            $lport = $offset + $i++;
+            exec(
+                "ssh -i $scriptpath/$name.sshkey -S $findflag " .
+                "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no " .
+                "-o ExitOnForwardFailure=yes -o ServerAliveInterval=20 -o ServerAliveCountMax=3 " .
+                "-fN -R $lport:localhost:$rport $name@$host > /dev/null 2>&1 &",
+                $output, $retval
+            );
+            if ($retval > 0) {
+                $output = implode("\n", $output);
+                if (DEBUG) { echo "Error: ($retval) - $output\n"; }
+                $response = file_get_contents( "$url?id=$id&error=1" );
+                killpids();
+                continue 2;
+            }
+        }
+
+        if (DEBUG) { echo "Waiting for tunnels to fork\n"; }
+        $success = false;
+        $retries = 0;
+        $max_retries = 10;
+        while (true) {
+            $pids = getpids();
+            if (count($pids) == count($ports)) {
+                $success = true;
+                break;
+            }
+            if (++$retries > $max_retries) {
+                break;
+            }
+            sleep(1);
+        }
+
+        if ($success) {
+            if (DEBUG) { echo "Tunneling success\n"; }
+            $response = file_get_contents( "$url?id=$id&connected=1" );
+        } else {
+            if (DEBUG) { echo "Tunneling failed\n"; }
+            killpids();
+            $response = file_get_contents( "$url?id=$id&error=1" );
+        }
+
+        # once we're connected, we poll quickly so we can
+        # pick up disconnect messages quickly
+        $interval = $min_interval;
+
+    } else if ($response == "DISCONNECT") {
+
+        killpids();
+        $response = file_get_contents( "$url?id=$id&disconnected=1" );
+
+    # after disconnecting, we go back to the standard polling rate
+        $interval = $std_interval;
+        
+    }
+
+# XXX We don't bother checking the pids for now -- until we get around
+# to writing the code to do something useful with lost connections.
+# Instead we let ssh keepalives and timeouts clean things up and it
+# may require us to issue a disconnect/reconnect in the admin interface.
+#    if ($pids) {
+#        echo "CHECKING pids:\n";
+#        $newpids = array();
+#        foreach ($pids as $pid) {
+#            echo "\t$pid...";
+#            if (!$pid || ! file_exists("/proc/$pid")) {
+#                $response = file_get_contents( "$url?id=$id&error=1" );
+#                echo "GONE.\n";
+#            } else {
+#                echo "OK.\n";
+#                array_push($newpids, $pid);
+#            }
+#        }
+#        # we refill it with only the pids that still existed
+#        $pids = $newpids;
+#    }
+
+    # and now we chill
+    sleep($interval);
+
+}
+
+function getpids() {
+
+    global $findflag;
+
+    # the ' ? ' detects the forked/detached tunnel, as opposed to the
+    # short-lived process that forks it off (they otherwise look the same)
+    exec("ps x | grep $findflag | grep ' ? ' | grep -v grep", $output, $retval);
+
+    $pids = array();
+    if ($output) {
+        foreach ($output as $proc) {
+            $pid = preg_replace("/^ *(\d+).*/", "$1", $proc);
+            array_push($pids, $pid);
+        }
+    }
+
+    return $pids;
+
+}
+
+function killpids() {
+    $pids = getpids();
+    if ($pids) {
+        foreach ($pids as $pid) {
+            if (DEBUG) { echo "Killing $pid...\n"; }
+            exec("kill $pid");
+            # check result?
+        }
+    }
+}
+
+?>
