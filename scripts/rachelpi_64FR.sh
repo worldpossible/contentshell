42831780ae0dc70bc63e704ca8afb049dd20f94d
Jonathan Field <jfield@worldpossible.org>
2017-06-22 21:23:02 +0100

    we no longer use these scripts

--- scripts/rachelpi_64FR.sh
+++
@@ -1,8 +0,0 @@
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-ebooksgratuits /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-haitifutur /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-kalite /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-wikipedia /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-wikivoyage /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-wiktionary /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/en-worldmap /var/www/modules
-sudo rsync -Pavz rsync://dev.worldpossible.org/rachelmods/fr-windows_apps /var/www/modules
