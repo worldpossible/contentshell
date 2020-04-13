133f2a9e590f7fe4493815db52a3b4a31e4e9b45
worldpossible <steve@worldpossible.org>
2020-04-13 16:10:51 -0700

    Add files

---
+++ Key_Init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# --------------------------------------------------------------------------------------
+# Script: Key_Init.sh | Rev: 1.0
+# Author: Steve Bashford | Email: Steve@AnalyticsOcean.Net
+# Action: Adds ssh key to the current bash terminal (not a perminent solution)
+# --------------------------------------------------------------------------------------
+
+# Add key:
+  #sudo ssh-agent bash
+  ssh-add /home/wp/.ssh/id_git
